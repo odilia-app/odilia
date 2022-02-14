f3d3a724a1672ee147b3d11b9717c6ea2749c800
Michael Connor Buchan <mikey@blindcomputing.org>
2022-02-14 08:41:17 +0000

    Bin: Add more documentation

--- src/logging.rs
+++ src/logging.rs
@@ -1,6 +1,9 @@
+//!Logging with the [`tracing`] crate.
+//!
 //! Not much here yet, but this will get more complex if we decide to add other layers for error
 //! reporting, tokio-console, etc.
 
+/// Initialise the logging stack. Right now this just calls [`tracing_subscriber::fmt::init`].
 pub fn init() {
     tracing_subscriber::fmt::init();
 }
