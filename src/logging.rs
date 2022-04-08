684d1daf61a50bd63817ed91e65cc778c856c536
Michael Connor Buchan <mikey@blindcomputing.org>
2022-04-08 09:50:58 +0100

    Use eyre and color-eyre for error and panic handling
    I chose eyre because it integrates nicely with tracing, using the
    color_eyre and tracing_error crates. It has feature parity with anyhow, and is widely used
    by the Rust community (over 3 million downloads from crates.io).

--- src/logging.rs
+++ src/logging.rs
@@ -3,7 +3,18 @@
 //! Not much here yet, but this will get more complex if we decide to add other layers for error
 //! reporting, tokio-console, etc.
 
+use tracing_error::ErrorLayer;
+use tracing_subscriber::{fmt, prelude::*};
+
 /// Initialise the logging stack. Right now this just calls [`tracing_subscriber::fmt::init`].
 pub fn init() {
-    tracing_subscriber::fmt::init();
+    let subscriber = tracing_subscriber::Registry::default()
+        .with(fmt::layer())
+        .with(ErrorLayer::default());
+    if let Err(e) = tracing::subscriber::set_global_default(subscriber) {
+        eprintln!("Warning: Failed to set log handler: {}", e);
+    }
+    if let Err(e) = color_eyre::install() {
+        tracing::warn!(error = %e, "Failed to install error / panic handler");
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ mod args;
 mod logging;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> eyre::Result<()> {
     logging::init();
     let _args = args::parse();
     tracing::info!("Hello, world!");
+    Ok(())
 }
