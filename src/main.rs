aaa75b43075f7cd55ee711a6eb21cc0a05a00e2a
Michael Connor Buchan <mikey@blindcomputing.org>
2022-04-17 12:33:13 +0100

    Bin: Start work on a ScreenReaderState
    Currently this just holds the atspi::Connection.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 mod args;
 mod logging;
+mod state;
+use state::ScreenReaderState;
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     logging::init();
     let _args = args::parse();
+    let _state = ScreenReaderState::new().await?;
     tracing::info!("Hello, world!");
     Ok(())
 }
---
+++ src/state.rs
@@ -0,0 +1,12 @@
+use eyre::WrapErr;
+
+pub struct ScreenReaderState {
+    pub atspi: atspi::Connection,
+}
+
+impl ScreenReaderState {
+    pub async fn new() -> eyre::Result<Self> {
+        let atspi = atspi::Connection::open().await.context("Could not connect to at-spi bus")?;
+        Ok(Self { atspi })
+    }
+}
