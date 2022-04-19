401cd72e3426a8e420a4c1b7eceac1984883798b
Michael Connor Buchan <mikey@blindcomputing.org>
2022-04-19 02:34:55 +0100

    Honour ODILIA_LOG and RUST_LOG env vars to set log filter

--- src/logging.rs
+++ src/logging.rs
@@ -3,13 +3,32 @@
 //! Not much here yet, but this will get more complex if we decide to add other layers for error
 //! reporting, tokio-console, etc.
 
+use std::env;
+
 use tracing_error::ErrorLayer;
 use tracing_log::LogTracer;
-use tracing_subscriber::{fmt, prelude::*};
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
-/// Initialise the logging stack. Right now this just calls [`tracing_subscriber::fmt::init`].
+#[cfg(not(release))]
+const DEFAULT_LOG_FILTER: &'static str = "debug";
+#[cfg(release)]
+const DEFAULT_LOG_FILTER: &'static str = "error";
+
+/// Initialise the logging stack.
 pub fn init() {
+    let env_filter = match env::var("ODILIA_LOG").or_else(|_| env::var("RUST_LOG")) {
+        Ok(s) => EnvFilter::from(s),
+        Err(env::VarError::NotPresent) => EnvFilter::from(DEFAULT_LOG_FILTER),
+        Err(e) => {
+            eprintln!(
+                "Warning: Failed to read log filter from ODILIA_LOG or RUST_LOG: {}",
+                e
+            );
+            EnvFilter::from(DEFAULT_LOG_FILTER)
+        }
+    };
     let subscriber = tracing_subscriber::Registry::default()
+        .with(env_filter)
         .with(fmt::layer())
         .with(ErrorLayer::default());
     if let Err(e) = tracing::subscriber::set_global_default(subscriber) {
