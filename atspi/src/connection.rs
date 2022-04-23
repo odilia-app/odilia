ca785323a312d2cf0763ed43c46cfdf776f110fa
Michael Connor Buchan <mikey@blindcomputing.org>
2022-04-23 15:49:08 +0100

    Atspi: Allow connecting with a custom bus address
    We don't yet need this, but it may be useful for a command-line option,
    integration testing, etc.

--- atspi/src/connection.rs
+++ atspi/src/connection.rs
@@ -24,10 +24,12 @@ impl Connection {
     };
         tracing::debug!(address = %a11y_bus_addr, "Got a11y bus address");
         let addr: Address = a11y_bus_addr.parse()?;
+            Self::connect(addr).await
+    }
 
-        // Connect to the a11y bus
+    pub async fn connect(bus_addr: Address) -> zbus::Result<Self> {
          tracing::debug!("Connecting to a11y bus");
-        let bus = zbus::ConnectionBuilder::address(addr)?.build().await?;
+        let bus = zbus::ConnectionBuilder::address(bus_addr)?.build().await?;
         tracing::debug!(name = bus.unique_name().map(|n| n.as_str()), "Connected to a11y bus");
         // The Proxy holds a strong reference to a Connection, so we only need to store the proxy
         let registry = RegistryProxy::new(&bus).await?;
