0605fbeb01ddf9e773e70c1e29c800b51519a423
michael Connor Buchan <mikey@blindcomputing.org>
2022-05-24 18:16:26 +0100

    Bin: Begin to process events
    Currently this errors for each event, because the deserialization format
    is wrong. We initially took this from the autogenerated bindings, which
    itself comes from the XML, but it seems that XML is out-dated.

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,30 @@ mod logging;
 mod state;
 use state::ScreenReaderState;
 
+use futures::stream::StreamExt;
+
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     logging::init();
     let _args = args::parse();
-    let _state = ScreenReaderState::new().await?;
-    tracing::info!("Hello, world!");
+    let state = ScreenReaderState::new().await?;
+    state.register_event("Object:StateChanged:Focused").await?;
+    process_events(&state).await;
     Ok(())
+}
+
+#[tracing::instrument(level = "debug", skip(state))]
+async fn process_events(state: &ScreenReaderState) {
+    let events = state.atspi.event_stream();
+    pin_utils::pin_mut!(events);
+    while let Some(res) = events.next().await {
+        let event = match res {
+            Ok(e) => e,
+            Err(e) => {
+                tracing::error!(error = %e, "Error receiving atspi event");
+                continue;
+            },
+        };
+        tracing::debug!(kind = %event.kind(), "Got event");
+    }
 }
