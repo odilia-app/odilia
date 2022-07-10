3fa670eb08d55f38646c9c2afbf7c928ae2f7ba2
Michael Connor Buchan <mikey@lowerelements.club>
2022-07-10 20:21:13 +0100

    Begin work on dispatching events
    Currently we only dispatch Object:StateChanged:focused, and ignore the
    rest. Ignored events are logged.
    
    Currently events are processed serially. In future we plan to process
    events in parallel, however, simply spawning a new task for each event
    could lead to various issues and inefficiencies (I.E. lock contension,
    processing events that are obsilete due to the receipt of newer events,
    etc), so this will need to be implemented with care.

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,20 @@ use state::ScreenReaderState;
 
 use futures::stream::StreamExt;
 
+use atspi::events::Event;
+
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     logging::init();
     let _args = args::parse();
     let state = ScreenReaderState::new().await?;
     state.register_event("Object:StateChanged:Focused").await?;
-    process_events(&state).await;
+    process_events(&state).await?;
     Ok(())
 }
 
 #[tracing::instrument(level = "debug", skip(state))]
-async fn process_events(state: &ScreenReaderState) {
+async fn process_events(state: &ScreenReaderState) -> eyre::Result<()> {
     let events = state.atspi.event_stream();
     pin_utils::pin_mut!(events);
     while let Some(res) = events.next().await {
@@ -28,5 +30,38 @@ async fn process_events(state: &ScreenReaderState) {
             }
         };
         tracing::debug!(kind = %event.kind(), "Got event");
+        // Dispatch based on interface
+        if let Some(interface) = event.interface() {
+        match interface.rsplit('.').next().expect("Interface name should contain '.'") {
+            "Object" => process_object_event(state, event).await?,
+            interface => tracing::debug!(interface, "Ignoring event with unknown interface"),
     }
+    }
+    }
+    Ok(())
+}
+
+async fn process_object_event(state: &ScreenReaderState, event: Event) -> eyre::Result<()> {
+    // Dispatch based on member
+    if let Some(member) = event.member() {
+    match member.as_str() {
+        "StateChanged" => process_state_changed_event(state, event).await?,
+            member => tracing::debug!(member, "Ignoring event with unknown member"),
+    }
+    }
+Ok(())
+}
+
+async fn process_state_changed_event(state: &ScreenReaderState, event: Event) -> eyre::Result<()> {
+    // Dispatch based on kind
+    match event.kind() {
+        "focused" => process_focused_event(state, event).await?,
+            kind => tracing::debug!(kind, "Ignoring event with unknown kind"),
+    }
+    Ok(())
+}
+
+async fn process_focused_event(state: &ScreenReaderState, event: Event) -> zbus::Result<()> {
+    // Speak the newly focused object
+    Ok(())
 }
