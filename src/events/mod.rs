9ad547231d289d325373a767cb6567fd4d7a09f6
Tait Hoyem <tait@tait.tech>
2022-08-01 20:56:00 -0600

    Use role and direction variants for function parameters

--- src/events/mod.rs
+++ src/events/mod.rs
@@ -2,7 +2,10 @@ mod object;
 
 use speech_dispatcher::Priority;
 use odilia_common::{
-    events::ScreenReaderEvent,
+    events::{
+      ScreenReaderEvent,
+      Direction
+    },
     modes::ScreenReaderMode,
 };
 use futures::{
@@ -27,6 +30,22 @@ use crate::state;
 use crate::state::{
   ScreenReaderState
 };
+type AsyncFn =
+  Box<dyn Fn(AccessibleProxy<'static>) -> Box<dyn Future<Output=zbus::Result<bool>> + Unpin + Send + 'static> + Send + Sync + 'static>;
+
+/* TODO: I'm pretty sure this requires three heap allocation for every call.
+* The good news is that it only *needs* to be called once for every possible match. Grrrrr. I wish this could be done at compile-time with macros. So much more efficient.
+* Also, right now the heap allocation is done every time the key is pressed.
+*/
+pub fn matchr(role: Role) -> AsyncFn {
+  Box::new(move |acc: AccessibleProxy<'static>| {
+    Box::new(Box::pin(
+      async move {
+        Ok(acc.get_role().await? == role)
+      }
+    ))
+  })
+}
 
 pub async fn match_heading(acc: AccessibleProxy<'_>) -> zbus::Result<bool> {
   Ok(acc.get_role().await? == Role::Heading)
@@ -37,8 +56,13 @@ pub async fn sr_event(sr_events: &mut Receiver<ScreenReaderEvent>, mode_channel:
     while let Some(sr_event) = sr_events.recv().await {
         let event_result = match sr_event {
             ScreenReaderEvent::StructuralNavigation(dir,role) => {
+              let match_func = matchr(role);
               let curr = state::get_accessible_history(0).await?;
-              if let Some(next) = curr.get_next(match_heading, false).await? {
+              let direction = match dir {
+                Direction::Forward => false,
+                Direction::Backward => true,
+              };
+              if let Some(next) = curr.get_next(&matchr(role), direction).await? {
                 let text = next.to_text().await?;
                 text.set_caret_offset(0).await?;
               } else {
