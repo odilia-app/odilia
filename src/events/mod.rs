e86e219db39ecb83454356c18c2af732e6433749
Tait Hoyem <tait@tait.tech>
2022-08-06 12:47:02 -0600

    Error checking for events; now at least prints an error for QT apps

--- src/events/mod.rs
+++ src/events/mod.rs
@@ -10,12 +10,38 @@ use tokio::sync::mpsc::{Receiver, Sender};
 
 use crate::state;
 use atspi::{
+    accessible::Role,
     accessible_plus::{AccessiblePlus, MatcherArgs},
     collection::MatchType,
     convertable::Convertable,
     events::Event,
 };
 use std::collections::HashMap;
+pub async fn structural_navigation(dir: Direction, role: Role) -> zbus::Result<()> {
+    let curr = state::get_accessible_history(0).await?;
+    let direction = match dir {
+        Direction::Forward => false,
+        Direction::Backward => true,
+    };
+    let roles = vec![role];
+    let attributes = HashMap::new();
+    let interfaces = Vec::new();
+    let mt: MatcherArgs = (
+        roles,
+        MatchType::Invalid,
+        attributes,
+        MatchType::Invalid,
+        interfaces,
+        MatchType::Invalid,
+    );
+    if let Some(next) = curr.get_next(&mt, direction).await? {
+        let text = next.to_text().await?;
+        text.set_caret_offset(0).await?;
+    } else {
+        state::say(Priority::Text, "No more headings".to_string()).await;
+    }
+    Ok(())
+}
 
 pub async fn sr_event(
     sr_events: &mut Receiver<ScreenReaderEvent>,
@@ -25,29 +51,12 @@ pub async fn sr_event(
     while let Some(sr_event) = sr_events.recv().await {
         let _event_result = match sr_event {
             ScreenReaderEvent::StructuralNavigation(dir, role) => {
-                let curr = state::get_accessible_history(0).await?;
-                let direction = match dir {
-                    Direction::Forward => false,
-                    Direction::Backward => true,
-                };
-                let roles = vec![role];
-                let attributes = HashMap::new();
-                let interfaces = Vec::new();
-                let mt: MatcherArgs = (
-                    roles,
-                    MatchType::Invalid,
-                    attributes,
-                    MatchType::Invalid,
-                    interfaces,
-                    MatchType::Invalid,
-                );
-                if let Some(next) = curr.get_next(&mt, direction).await? {
-                    let text = next.to_text().await?;
-                    text.set_caret_offset(0).await?;
+                if let Ok(_) = structural_navigation(dir, role).await {
+                    // it worked!
                 } else {
-                    state::say(Priority::Text, "No more headings".to_string()).await;
+                    tracing::debug!("There was an error with the structural navigation call.");
                 }
-            }
+            },
             ScreenReaderEvent::StopSpeech => println!("Stop speech!"),
             ScreenReaderEvent::ChangeMode(ScreenReaderMode { name }) => {
                 tracing::debug!("Change mode to {:?}", name);
@@ -63,11 +72,16 @@ pub async fn sr_event(
 pub async fn process() {
     let events = state::get_event_stream().await;
     pin_utils::pin_mut!(events);
-    while let Some(Ok(event)) = events.next().await {
-        if let Err(e) = dispatch(event).await {
-            tracing::error!(error = %e, "Could not handle event");
-        } else {
-            tracing::debug!("Event handled without error");
+    while let result = events.next().await {
+        match result {
+            Some(Ok(event)) => {
+                if let Err(e) = dispatch(event).await {
+                    tracing::error!(error = %e, "Could not handle event");
+                } else {
+                    tracing::debug!("Event handled without error");
+                }
+            },
+            _ => tracing::debug!("Event is none"),
         }
     }
 }
