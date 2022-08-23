df26814fb8b0834d65cc06e9a2e04c19a459bf18
Michael Connor Buchan <mikey@blindcomputing.org>
2022-08-23 18:06:45 +0100

    Bin: Refactor main()
    Add more comments, use a static array instead of a HashMap for
    structural navigation keybindings.

--- odilia/src/main.rs
+++ odilia/src/main.rs
@@ -23,73 +23,48 @@ async fn main() -> eyre::Result<()> {
     logging::init();
     let _args = args::parse();
 
-    // Setup structural navigation
-    let mut s_nav = HashMap::new();
-    s_nav.insert(Some(Key::Other('h')), Role::Heading);
-    s_nav.insert(Some(Key::Other('b')), Role::PushButton);
-    s_nav.insert(Some(Key::Other('k')), Role::Link);
-    s_nav.insert(Some(Key::Other('l')), Role::List);
-    s_nav.insert(Some(Key::Other('i')), Role::ListItem);
-    let ctrl = KeyBinding {
-        key: None,
-        mods: Modifiers::CONTROL,
-        repeat: 1,
-        consume: false,
-        mode: None,
-        notify: true,
-    };
-    let noop_caps = KeyBinding {
-        key: None,
-        mods: Modifiers::ODILIA,
-        repeat: 1,
-        consume: true,
-        mode: None,
-        notify: false,
-    };
-    let browse_mode = KeyBinding {
-        key: Some(Key::Other('b')),
-        mods: Modifiers::ODILIA,
-        repeat: 1,
-        consume: true,
-        mode: None,
-        notify: true,
-    };
+    // Initialize state
+    state::init_state().await?;
 
     let (mode_change_tx, mut mode_change_rx) = channel(8); // should maybe be 1? I don't know how it works
+
     let mut screen_reader_event_stream = create_keybind_channel();
-    for (key, role) in s_nav.into_iter() {
-        let kb = KeyBinding {
-            key: key,
-            mods: Modifiers::NONE,
-            repeat: 1,
-            consume: true,
-            mode: Some(ScreenReaderMode {
-                name: "BrowseMode".to_string(),
-            }),
-            notify: true,
-        };
-        let bkb = KeyBinding {
-            key: key,
-            mods: Modifiers::SHIFT,
-            repeat: 1,
-            consume: true,
-            mode: Some(ScreenReaderMode {
-                name: "BrowseMode".to_string(),
-            }),
-            notify: true,
-        };
+
+    // Add directional structural nav keys
+    const S_NAV_BINDINGS: &'static [(Key, Role)]= &[
+    (Key::Other('h'), Role::Heading),
+    (Key::Other('b'), Role::PushButton),
+    (Key::Other('k'), Role::Link),
+    (Key::Other('l'), Role::List),
+    (Key::Other('i'), Role::ListItem),
+    ];
+
+    for (key, role) in S_NAV_BINDINGS.iter().copied() {
+        let forward_kb = KeyBinding::new(Some(key)).mode(Some(ScreenReaderMode {
+            name: "BrowseMode".to_string(),
+        }));
+        let backward_kb = forward_kb.clone().mods(Modifiers::SHIFT);
+
         add_keybind(
-            kb,
+            forward_kb,
             ScreenReaderEvent::StructuralNavigation(Direction::Forward, role),
         )
         .await;
         add_keybind(
-            bkb,
+            backward_kb,
             ScreenReaderEvent::StructuralNavigation(Direction::Backward, role),
         )
         .await;
     }
+
+    // Misc keybindings
+    let noop_caps = KeyBinding::default().mods(Modifiers::ODILIA).notify(false);
+    add_keybind(noop_caps, ScreenReaderEvent::Noop).await;
+    let ctrl = KeyBinding::default()
+        .mods(Modifiers::CONTROL)
+        .consume(false);
     add_keybind(ctrl, ScreenReaderEvent::StopSpeech).await;
+    let browse_mode = KeyBinding::new(Some(Key::Other('b'))).mods(Modifiers::ODILIA);
     add_keybind(
         browse_mode,
         ScreenReaderEvent::ChangeMode(ScreenReaderMode {
@@ -97,10 +72,6 @@ async fn main() -> eyre::Result<()> {
         }),
     )
     .await;
-    add_keybind(noop_caps, ScreenReaderEvent::Noop).await;
-
-    // Initialize state
-    state::init_state().await?;
 
     // Register events
     state::register_event("Object:StateChanged:Focused").await?;
