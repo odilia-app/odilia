855aef3b0535c884e4e6f86b0494ad4d9e27bfe4
Tait Hoyem <tait@tait.tech>
2022-09-04 17:51:30 -0600

    add dynamic 'no more X' responses when structural navigation does not find a valid role'

--- common/src/events.rs
+++ common/src/events.rs
@@ -13,12 +13,14 @@ pub enum Feature {
 }
 
 #[derive(Eq,PartialEq,Clone,Hash,Serialize,Deserialize)]
+#[serde(tag="direction")]
 pub enum Direction {
     Forward,
     Backward,
 }
 
 #[derive(Eq,PartialEq,Clone,Hash,Serialize,Deserialize)]
+#[serde(tag="event", content="args", rename_all="camelCase")]
 pub enum ScreenReaderEvent {
     Noop, // when we need to do "something" but this is always hardcoded as nothing
     Speak(String, u32),
