f59d106a18ac07bf4e61d55168e27d293db1c740
Tait Hoyem <tait@tait.tech>
2022-09-08 13:45:03 -0600

    Use line granularity on caret moved events

--- odilia/src/events/object.rs
+++ odilia/src/events/object.rs
@@ -20,12 +20,7 @@ mod text_caret_moved {
     use crate::state::ScreenReaderState;
 
     pub async fn text_cursor_moved(state: &ScreenReaderState, event: Event) -> eyre::Result<()> {
-        let last_caret_pos = state.previous_caret_position.get();
         let current_caret_pos = event.detail1();
-
-        let start = min(last_caret_pos, current_caret_pos);
-        let end = max(last_caret_pos, current_caret_pos);
-
         let path = if let Some(path) = event.path() {
             path
         } else {
@@ -39,12 +34,27 @@ mod text_caret_moved {
         let conn = state.connection();
         let text = text::new(&conn.clone(), sender.to_owned(), path.to_owned()).await?;
         let accessible = accessible::new(&conn, sender.clone(), path.clone()).await?;
-        let name = text.get_text(start, end).await?;
+        if let Some(latest) = state.history_item(0).await? {
+          if let Some(second) = state.history_item(1).await? {
+          if latest == accessible && second != accessible && current_caret_pos == 0 {
+              tracing::trace!("Caret is moved to latest accessible and the second latest isn't the same and te cursor is at zero; this is usually a result of a structural navigation or tab. Do not read out.");
+              state.update_accessible(sender, path).await;
+              return Ok(());
+            }
+          }
+        }
+        let line = text.get_string_at_offset(current_caret_pos, text::TextGranularity::Line).await?.0;
         state.update_accessible(sender, path).await;
 
-        // this just won't work on the first two accessibles we call. oh well.
-        let latest_accessible = state.history_item(0).await?;
-        let second_latest_accessible = state.history_item(0).await?;
+        // TODO this just won't work on the first two accessibles we call. oh well.
+        let latest_accessible = match state.history_item(0).await? {
+          Some(acc) => acc,
+          None => return Ok(()),
+        };
+        let second_latest_accessible = match state.history_item(0).await? {
+          Some(acc) => acc,
+          None => return Ok(())
+        };
         // if this is the same accessible as previously acted upon, and caret position is 0
         // This will be true if the user has just tabbed into a new accessible.
         if latest_accessible.path() == accessible.path()
@@ -54,9 +64,9 @@ mod text_caret_moved {
             tracing::debug!("Tabbed selection detected. Do no re-speak due to caret navigation.");
         } else {
             tracing::debug!("Tabbed selection not detected.");
-            if !name.is_empty() {
-                tracing::debug!("Speaking normal caret navigation");
-                state.say(speech_dispatcher::Priority::Text, name).await;
+            tracing::debug!("Speaking normal caret navigation");
+            if !line.is_empty() {
+              state.say(speech_dispatcher::Priority::Text, format!("{}", line)).await;
             }
         }
 
@@ -102,12 +112,17 @@ mod state_changed {
         };
         let conn = state.connection();
         let accessible = accessible::new(&conn.clone(), sender.clone(), path.clone()).await?;
-
+        if let Some(curr) = state.history_item(0).await? {
+          if curr == accessible {
+            return Ok(());
+          }
+        }
         state.update_accessible(sender.to_owned(), path.to_owned()).await;
 
         let name = accessible.name().await?;
         let description = accessible.description().await?;
         let role = accessible.get_localized_role_name().await?;
+        tracing::debug!("Focus event received on: {} with role {}", path, role);
         let relation = accessible.get_relation_set().await?;
         tracing::debug!("Relations: {:?}", relation);
 
