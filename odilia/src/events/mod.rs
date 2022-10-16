c2e4301accb51af6abe7f80c44280e8c592ce71f
Tait Hoyem <tait@tait.tech>
2022-10-16 15:17:07 -0600

    Remove speech-dispatcher dependency

--- odilia/src/events/mod.rs
+++ odilia/src/events/mod.rs
@@ -5,7 +5,10 @@ use std::{collections::HashMap, rc::Rc};
 
 use futures::stream::StreamExt;
 use speech_dispatcher::Priority;
-use tokio::sync::mpsc::{Receiver, Sender};
+use tokio::sync::{
+    mpsc::{Receiver, Sender},
+    broadcast,
+};
 
 use atspi::{
     accessible::Role,
@@ -60,54 +63,75 @@ pub async fn structural_navigation(state: &ScreenReaderState, dir: Direction, ro
 pub async fn sr_event(
     state: Rc<ScreenReaderState>,
     sr_events: &mut Receiver<ScreenReaderEvent>,
+    shutdown_rx: &mut broadcast::Receiver<i32>,
 ) -> zbus::Result<()> {
     loop {
-        let sr_event = match sr_events.recv().await {
-            Some(e) => e,
-            _ => continue
-        };
-        tracing::debug!("SR Event received");
-        match sr_event {
-            ScreenReaderEvent::StructuralNavigation(dir, role) => {
-                 if let Err(e) = structural_navigation(&state, dir, role).await {
-                    tracing::debug!(error = %e, "There was an error with the structural navigation call.");
-                }
-            },
-            ScreenReaderEvent::StopSpeech => tracing::trace!("Stopping speech!"),
-            ScreenReaderEvent::ChangeMode(ScreenReaderMode { name }) => {
-                tracing::debug!("Changing mode to {:?}", name);
-                //let _ = mode_channel.send(ScreenReaderMode { name }).await;
+        tokio::select! {
+            Some(sr_event) = sr_events.recv() => {
+                tracing::debug!("SR Event received");
+                match sr_event {
+                    ScreenReaderEvent::StructuralNavigation(dir, role) => {
+                         if let Err(e) = structural_navigation(&state, dir, role).await {
+                            tracing::debug!(error = %e, "There was an error with the structural navigation call.");
+                        }
+                    },
+                    ScreenReaderEvent::StopSpeech => tracing::trace!("Stopping speech!"),
+                    ScreenReaderEvent::ChangeMode(ScreenReaderMode { name }) => {
+                        tracing::debug!("Changing mode to {:?}", name);
+                        //let _ = mode_channel.send(ScreenReaderMode { name }).await;
+                    }
+                    _ => {}
+                };
             }
-            _ => {}
-        };
+            Ok(i) = shutdown_rx.recv() => {
+                tracing::debug!("sr_event cancelled");
+                break;
+            }
+        }
     }
+    Ok(())
 }
 
 //#[tracing::instrument(level = "debug"i, skip(state))]
-pub async fn receive(state: Rc<ScreenReaderState>, tx: Sender<Event>) {
+pub async fn receive(state: Rc<ScreenReaderState>, tx: Sender<Event>, shutdown_rx: &mut broadcast::Receiver<i32>) {
     let events = state.atspi.event_stream();
     pin_utils::pin_mut!(events);
     loop {
-        match events.next().await {
-            Some(Ok(event)) => {
-              if let Err(e) = tx.send(event).await {
-                tracing::error!(error = %e, "Error sending atspi event");
-              }
-            },
-            _ => tracing::debug!("Event is none"),
+        tokio::select! {
+            i = shutdown_rx.recv() => {
+                tracing::debug!("receive function is done");
+                break;
+            }
+            event = events.next() => {
+                if let Some(Ok(good_event)) = event {
+                    if let Err(e) = tx.send(good_event).await {
+                        tracing::error!(error = %e, "Error sending atspi event");
+                    } 
+                } else {
+                    tracing::debug!("Event is either None or an Error variant.");
+                }
+            }
         }
     }
 }
 
 //#[tracing::instrument(level = "debug")]
-pub async fn process(state: Rc<ScreenReaderState>, rx: &mut Receiver<Event>) {
-  while let Some(event) = rx.recv().await {
-    if let Err(e) = dispatch(&state, event).await {
-        tracing::error!(error = %e, "Could not handle event");
-    } else {
-        tracing::debug!("Event handled without error");
+pub async fn process(state: Rc<ScreenReaderState>, rx: &mut Receiver<Event>, shutdown_rx: &mut broadcast::Receiver<i32>) {
+    loop {
+        tokio::select! {
+            Some(event) = rx.recv() => {
+                if let Err(e) = dispatch(&state, event).await {
+                    tracing::error!(error = %e, "Could not handle event");
+                } else {
+                    tracing::debug!("Event handled without error");
+                }
+            }
+            i = shutdown_rx.recv() => {
+                tracing::debug!("process function is done");
+                break;
+            }
+        }
     }
-  }
 }
 
 async fn dispatch(state: &ScreenReaderState, event: Event) -> eyre::Result<()> {
--- odilia/src/main.rs
+++ odilia/src/main.rs
@@ -8,7 +8,15 @@ use std::rc::Rc;
 
 use eyre::WrapErr;
 use futures::future::FutureExt;
-use tokio::sync::mpsc::channel;
+use tokio::{
+    sync::mpsc,
+    sync::broadcast,
+    signal::unix::{
+        SignalKind,
+        signal,
+    },
+};
+use tokio_graceful_shutdown::{SubsystemHandle, Toplevel};
 
 use atspi::accessible::Role;
 use crate::state::ScreenReaderState;
@@ -19,31 +27,54 @@ use odilia_common::{
 use odilia_input::sr_event_receiver;
 use serde_json;
 
+async fn sigterm_signal_watcher(state: Rc<ScreenReaderState>, shutdown_tx: broadcast::Sender<i32>) -> eyre::Result<()>{
+    let mut c = signal(SignalKind::interrupt())?;
+    tracing::debug!("Watching for Ctrl+C");
+    loop {
+        c.recv().await;
+        shutdown_tx.send(0);
+        tracing::debug!("Asking all processes to stop.");
+        return Ok(());
+    }
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> eyre::Result<()> {
     logging::init();
     let _args = args::parse();
     let change_mode = ScreenReaderEvent::ChangeMode(ScreenReaderMode{ name: "Browse".to_string()});
     let sn = ScreenReaderEvent::StructuralNavigation(Direction::Forward, Role::Heading);
-    tracing::debug!("CM {:?}", serde_json::to_string(&change_mode).unwrap());
-    tracing::debug!("SN {:?}", serde_json::to_string(&sn).unwrap());
+    let (shutdown_tx, _) = broadcast::channel(1);
     // Initialize state
     let state = Rc::new(ScreenReaderState::new().await?);
 
-    let (sr_event_tx, mut sr_event_rx) = channel(8);
+    let (sr_event_tx, mut sr_event_rx) = mpsc::channel(8);
     // this channel must NEVER fill up; it will cause the thread receiving events to deadlock due to a zbus design choice.
     // If you need to make it bigger, then make it bigger, but do NOT let it ever fill up.
-    let (atspi_event_tx, mut atspi_event_rx) = channel(64);
+    let (atspi_event_tx, mut atspi_event_rx) = mpsc::channel(64);
 
     // Register events
     state.register_event("Object:StateChanged:Focused").await?;
     state.register_event("Object:TextCaretMoved").await?;
     state.register_event("Document:LoadComplete").await?;
 
-    let atspi_event_receiver = events::receive(Rc::clone(&state), atspi_event_tx).map(|_| Ok::<_, eyre::Report>(()));
-    let atspi_event_processor = events::process(Rc::clone(&state), &mut atspi_event_rx).map(|_| Ok::<_, eyre::Report>(()));
-    let odilia_event_receiver = sr_event_receiver(sr_event_tx).map(|r| r.wrap_err("Could not process Odilia events"));
-    let odilia_event_processor = events::sr_event(Rc::clone(&state), &mut sr_event_rx).map(|r| r.wrap_err("Could not process Odilia event"));
-    tokio::try_join!(atspi_event_receiver, atspi_event_processor, odilia_event_receiver, odilia_event_processor)?;
+    let mut shutdown_rx_atspi_recv = shutdown_tx.subscribe();
+    let atspi_event_receiver = events::receive(Rc::clone(&state), atspi_event_tx, &mut shutdown_rx_atspi_recv).map(|_| Ok::<_, eyre::Report>(()));
+    let mut shutdown_rx_atspi_proc_recv = shutdown_tx.subscribe();
+    let atspi_event_processor = events::process(Rc::clone(&state), &mut atspi_event_rx, &mut shutdown_rx_atspi_proc_recv).map(|_| Ok::<_, eyre::Report>(()));
+    let mut shutdown_rx_odilia_recv = shutdown_tx.subscribe();
+    let odilia_event_receiver = sr_event_receiver(sr_event_tx, &mut shutdown_rx_odilia_recv).map(|r| r.wrap_err("Could not process Odilia events"));
+    let mut shutdown_rx_odilia_proc_recv = shutdown_tx.subscribe();
+    let odilia_event_processor = events::sr_event(Rc::clone(&state), &mut sr_event_rx, &mut shutdown_rx_odilia_proc_recv).map(|r| r.wrap_err("Could not process Odilia event"));
+    let signal_receiver = sigterm_signal_watcher(Rc::clone(&state), shutdown_tx).map(|r| r.wrap_err("Could not process signal shutdown."));
+    tokio::try_join!(signal_receiver, atspi_event_receiver, atspi_event_processor, odilia_event_receiver, odilia_event_processor)?;
+    tracing::debug!("All listeners have stopped. Running cleanup code.");
+    let _ = state.speaker.cancel_all();
+    if let Ok(_) = state.speaker.stop() {
+        tracing::debug!("Speech-dispatcher has successfully been stopped.");
+    } else {
+        tracing::debug!("Speech-dispatched has not been stopped; you may see problems when attempting to use it again.");
+    }
+    tracing::debug!("Goodbye, Odilia!");
     Ok(())
 }
