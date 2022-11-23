ff07e7febc6d9de1cd701ff05307120b8833eb0f
Tait Hoyem <tait@tait.tech>
2022-11-22 19:03:07 -0700

    Reference atspi by its own, separate library

--- atspi/src/events/focus.rs
+++
@@ -1,8 +0,0 @@
-use zbus::dbus_proxy;
-
-#[dbus_proxy(interface = "org.a11y.atspi.Event.Focus")]
-trait Focus {
-    /// Focus signal
-    #[dbus_proxy(signal)]
-    fn focus(&self, event: super::EventBody<'_, &str>) -> zbus::Result<()>;
-}
--- atspi/src/events/keyboard.rs
+++
@@ -1,8 +0,0 @@
-use zbus::dbus_proxy;
-
-#[dbus_proxy(interface = "org.a11y.atspi.Event.Keyboard")]
-trait Keyboard {
-    /// Modifiers signal
-    #[dbus_proxy(signal)]
-    fn modifiers(&self, event: super::EventBody<'_, &str>) -> zbus::Result<()>;
-}
