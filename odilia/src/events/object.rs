f0d0d912d2a2e299dc6bd86c603fef0048ad326e
Tait Hoyem <tait@tait.tech>
2023-01-21 20:27:14 -0700

    Handle TextChanged events, update cache to reflect latest string information

--- odilia/src/events/object.rs
+++ odilia/src/events/object.rs
@@ -10,10 +10,189 @@ pub async fn dispatch(state: &ScreenReaderState, event: &ObjectEvents) -> eyre::
 	match event {
 		ObjectEvents::StateChanged(state_changed_event) => state_changed::dispatch(state, state_changed_event).await?,
 		ObjectEvents::TextCaretMoved(text_caret_moved_event) => text_caret_moved::dispatch(state, text_caret_moved_event).await?,
+		ObjectEvents::TextChanged(text_changed_event) => text_changed::dispatch(state, text_changed_event).await?,
 		ObjectEvents::ChildrenChanged(children_changed_event) => children_changed::dispatch(state, children_changed_event).await?,
 		other_member => tracing::debug!("Ignoring event with unknown member: {:#?}", other_member),
 	}
 	Ok(())
+}
+
+mod text_changed {
+	use std::collections::HashMap;
+	use odilia_cache::CacheItem;
+	use odilia_common::{
+		types::{
+			AriaLive,
+			AriaAtomic,
+		},
+		result::OdiliaResult,
+		errors::OdiliaError,
+	};
+	use crate::{
+		state::ScreenReaderState,
+	};
+	use atspi::{identify::object::TextChangedEvent, signify::Signified};
+	use ssip_client::Priority;
+
+	#[inline]
+	pub fn update_string_insert(start_pos: usize, update_length: usize, updated_text: &str) -> impl Fn(&mut CacheItem) + '_ {
+		move |cache_item| {
+			tracing::trace!("Insert into \"{}\"({:?} @ {}+{} should insert \"{}\"", cache_item.text, cache_item.object.id, start_pos, update_length, updated_text);
+			let char_num = cache_item.text.chars().count();
+			let prepend = start_pos == 0;
+			let append = start_pos >= char_num;
+			// if the end of the inserted string will go past the end of the original string
+			let insert_and_append = start_pos+update_length >= char_num;
+			cache_item.text = if prepend {
+				append_to_object(updated_text, &cache_item.text)
+			} else if append {
+				append_to_object(&cache_item.text, updated_text)
+			} else if insert_and_append {
+				insert_at_index(&cache_item.text, updated_text, start_pos)
+			} else {
+				insert_at_range(&cache_item.text, updated_text, start_pos, start_pos+update_length)
+			}
+		}
+	}
+
+	#[inline]
+	pub fn append_to_object(original: &str, to_append: &str) -> String {
+		let mut new_text = original.chars()
+			.collect::<Vec<char>>();
+		new_text.extend(to_append.chars());
+		new_text.into_iter().collect()
+	}
+
+	#[inline]
+	pub fn insert_at_index(original: &str, to_splice: &str, index: usize) -> String {
+		let mut new_text = original.chars()
+			.collect::<Vec<char>>();
+		new_text.splice(index.., to_splice.chars());
+		new_text.into_iter().collect()
+	}
+
+	#[inline]
+	pub fn insert_at_range(original: &str, to_splice: &str, start: usize, end: usize) -> String {
+		let mut new_text = original.chars()
+			.collect::<Vec<char>>();
+		new_text.splice(start..end, to_splice.chars());
+		new_text.into_iter().collect()
+	}
+
+	#[inline]
+	/// Get the live state of a set of attributes.
+	/// Although the function only currently tests one attributes, in the future it may be imporant to inspect many attributes, compare them, or do additional logic.
+	pub fn get_live_state(attributes: &HashMap<String, String>) -> OdiliaResult<AriaLive> {
+		match attributes.get("live") {
+			None => Err(OdiliaError::NoAttributeError("live".to_string())),
+			Some(live) => {
+				Ok(serde_plain::from_str(live)?)
+			}
+		}
+	}
+
+	#[inline]
+	/// if the aria-live attribute is set to "polite", then set the prioirty of the message to speak once all other messages are done
+	/// if the aria-live attribute is set to "assertive", thenset the priority of the message to speak immediately, stop all other messages, and do not interrupt that piece of speech
+	/// otherwise, do not continue
+	pub fn live_to_priority(live_str: AriaLive) -> Priority {
+		match live_str {
+			AriaLive::Assertive => Priority::Important,
+			AriaLive::Polite => Priority::Notification,
+			_ => Priority::Message,
+		}
+	}
+
+	#[inline]
+	pub fn get_atomic_state(attributes: &HashMap<String, String>) -> OdiliaResult<AriaAtomic> {
+		match attributes.get("atomic") {
+			None => Err(OdiliaError::NoAttributeError("atomic".to_string())),
+			Some(atomic) => {
+				Ok(serde_plain::from_str(atomic)?)
+			}
+		}
+	}
+
+	pub fn get_string_within_bounds(start_pos: usize, update_length: usize) -> impl Fn((usize, char)) -> Option<char> {
+		move |(index, chr)| {
+			let is_after_start = index >= start_pos;
+			let is_before_end = index <= start_pos + update_length;
+			if is_after_start && is_before_end {
+				Some(chr)
+			} else { None }
+		}
+	}
+
+	pub fn get_string_without_bounds(start_pos: usize, update_length: usize) -> impl Fn((usize, char)) -> Option<char> {
+		move |(index, chr)| {
+			let is_before_start = index < start_pos;
+			let is_after_end = index > start_pos + update_length;
+			if is_before_start || is_after_end {
+				Some(chr)
+			} else { None }
+		}
+	}
+
+	pub async fn dispatch(state: &ScreenReaderState, event: &TextChangedEvent) -> eyre::Result<()> {
+		let kind = event.kind();
+		match kind {
+			"insert/system" => insert_or_delete(state, event, true).await?,
+			"insert" => insert_or_delete(state, event, true).await?,
+			"delete/system" => insert_or_delete(state, event, false).await?,
+			"delete" => insert_or_delete(state, event, false).await?,
+			_ => tracing::trace!("TextChangedEvent has invalid kind: {}", kind),
+		};
+		Ok(())
+	}
+	
+	pub async fn speak_insertion(state: &ScreenReaderState, event: &TextChangedEvent, attributes: &HashMap<String, String>, cache_text: &str) -> OdiliaResult<()> {
+		// note, you should update the text before this happens, since this could potentially end the function
+		let live = get_live_state(attributes)?;
+		let atomic = get_atomic_state(attributes)?;
+		// if the atomic state is true, then read out the entite piece of text
+		// if atomic state is false, then only read out the portion which has been added
+		// otherwise, do not continue through this function
+		let text_to_say = match atomic {
+			true => cache_text.to_string(),
+			false => event.text().try_into()?,
+		};
+		let prioirty = live_to_priority(live);
+		state.say(prioirty, text_to_say).await;
+		Ok(())
+	}
+
+	/// The `insert` boolean, if set to true, will update the text in the cache.
+	/// If it is set to false, the selection will be removed.
+	/// The [`TextChangedEvent::kind`] value will *NOT* be checked by this function.
+	pub async fn insert_or_delete(state: &ScreenReaderState, event: &TextChangedEvent, insert: bool) -> eyre::Result<()> {
+		let accessible = state.new_accessible(event).await?;
+		let cache_item = state.cache.get_or_create(&accessible).await?;
+		let updated_text: String = event.text().try_into()?;
+		let current_text = cache_item.text;
+		let (start_pos, update_length) = (event.start_pos() as usize, event.length() as usize);
+		// if this is an insert, figure out if we shuld announce anything, then speak it;
+		// only after should we try to update the cache
+		if insert {
+			let attributes = accessible.get_attributes().await?;
+			let _ = speak_insertion(state, event, &attributes, &current_text).await;
+		}
+
+		let text_selection_from_cache: String = current_text.char_indices()
+			.filter_map(get_string_within_bounds(start_pos, update_length))
+			.collect();
+		let selection_matches_update = text_selection_from_cache == updated_text;
+		let insert_has_not_occured = insert && !selection_matches_update;
+		let remove_has_not_occured = !insert && selection_matches_update;
+		if insert_has_not_occured {
+			state.cache.modify_item(&item.object.id, update_string_insert(start_pos, update_length, &updated_text)).await;
+		} else if remove_has_not_occured {
+			state.cache.modify_item(&item.object.id, move |cache_item| {
+				cache_item.text = cache_item.text.char_indices()
+					.filter_map(get_string_without_bounds(start_pos, update_length)).collect();
+			}).await;
+		}
+		Ok(())
+	}
 }
 
 mod children_changed {
