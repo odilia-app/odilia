731581f4d58ee3273063886309f643793827507a
Tait Hoyem <tait@tait.tech>
2023-03-19 01:24:31 -0600

    Add one example of JSON you can pass over the Odilia socket

---
+++ input/examples/generate-json.rs
@@ -0,0 +1,20 @@
+use odilia_common::{
+	events::ScreenReaderEvent,
+	modes::ScreenReaderMode,
+};
+use serde_json;
+
+fn main() {
+	// a blank event that does nothing
+	let noop = ScreenReaderEvent::Noop;
+
+	// stop all current speech
+	let stop = ScreenReaderEvent::StopSpeech;
+
+	// change to an arbitrary mode
+	let mode_change = ScreenReaderEvent::ChangeMode(ScreenReaderMode::new("browse mode"));
+
+	println!("{}", serde_json::to_string(&noop).unwrap());
+	println!("{}", serde_json::to_string(&stop).unwrap());
+	println!("{}", serde_json::to_string(&mode_change).unwrap());
+}
