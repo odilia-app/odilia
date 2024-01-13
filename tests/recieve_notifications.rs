1be1da9a4df575832efbfacb4b877fc442d3b4db
Alberto Tirla <albertotirla@gmail.com>
2024-01-13 11:39:33 +0200

    add dev dependency to be able to send notifications correctly and modify the test accordingly
    the notify-rust crate is now a transitive dependency of odilia-notify, used for the integration test
    furthermore, the test now correctly sends a notification and should recieve it properly

--- tests/recieve_notifications.rs
+++ tests/recieve_notifications.rs
@@ -1,38 +1,11 @@
 use futures::TryStreamExt;
-use odilia_notify::*;
-use std::{collections::HashMap, error::Error, time::Duration};
-use zbus::{dbus_proxy, zvariant::Value, Connection};
+use odilia_notify::listen_to_dbus_notifications;
+use std::{error::Error, time::Duration};
 
-#[dbus_proxy(
-    interface = "org.freedesktop.Notifications",
-    default_service = "org.freedesktop.Notifications",
-    default_path = "/org/freedesktop/Notifications"
-)]
-trait FreedesktopNotifications {
-    #[allow(clippy::too_many_arguments)]
-    async fn notify(
-        &self,
-        app_name: &str,
-        replaces_id: u32,
-        app_icon: &str,
-        summary: &str,
-        body: &str,
-        actions: Vec<String>,
-        hints: HashMap<&str, Value<'_>>,
-        expire_timeout: i32,
-    ) -> Result<(), Box<dyn Error>>;
-}
+use notify_rust::Notification;
+
 #[tokio::test]
 async fn test_listen_to_dbus_notifications() -> Result<(), Box<dyn Error>> {
-    // Create a new connection
-    let connection = Connection::session().await.unwrap();
-    // Create a proxy for the org.freedesktop.Notifications interface
-    let proxy = FreedesktopNotificationsProxy::builder(&connection)
-        .destination("org.freedesktop.Notifications")
-        .unwrap()
-        .build()
-        .await?;
-
     // Spawn a new task to listen for notifications
     let listener_task = tokio::spawn(async move {
         let mut stream = listen_to_dbus_notifications().await;
@@ -49,18 +22,11 @@ async fn test_listen_to_dbus_notifications() -> Result<(), Box<dyn Error>> {
     tokio::time::sleep(Duration::from_secs(1)).await;
 
     // Send a Notification to see if it's correctly recieved on the other side
-    //this has been commented out for now, because the test fails in another way than intended with it enabled, incorrect signatures are to blame
-    //proxy
-    //        .notify(
-    //"test_app",
-    //0,
-    //"",
-    //"Test Summary",
-    //"Test Body",
-    //vec![],
-    //HashMap::new(),
-    //5000)
-    //.await.unwrap();
+Notification::new()
+.appname("test-notify")
+.summary("test summary")
+.body("test body")
+.show()?;
     // Await the listener task
     listener_task.await?.unwrap();
     Ok(())
