33b954d1ecb461b01c247e38e5a627162510ecee
alberto tirla <albertotirla@gmail.com>
2024-03-08 17:25:17 +0200

    add cancelation token for the notifications task
    now, in stead of a channel, the task recieves a cancelation token, monitoring that one. Not much of a change from the previous model, but at least the intent is clearer
    untill all tasks gain the ability to use the cancelation token, the sigint signal handler will have to support both options of shutting down
    
    add tokio-utils to workspace dependencies and make tts use cancelation tokens
    
    fix clippy warnings and add cancellation token to the at-spi event processor
    
    add cancellation tokens to another at-spi processor
    
    add cancellation tokens to the input processing handler
    
    add cancellation tokens to the screenreader events processor, the one which handles input events
    
    fix formatting problems reported by check
    
    remove the last use of the shutdown channel from the codebase, the sigint watcher now  only cancels that token
    
    Update ssip-client-async version
    
    Move Receives insated of mutably borrinwg them for the duration of the running of Odilia
    
    Update Cargo.lock
    
    Cargo format
    
    Fix clippy issues
    
    Update cargo lock

--- odilia/src/main.rs
+++ odilia/src/main.rs
@@ -15,25 +15,24 @@ mod state;
 
 use std::{process::exit, sync::Arc};
 
+use crate::cli::Args;
+use crate::state::ScreenReaderState;
 use clap::Parser;
 use eyre::WrapErr;
 use futures::{future::FutureExt, StreamExt};
-use tokio::{
-	signal::unix::{signal, SignalKind},
-	sync::broadcast::{self, error::SendError},
-	sync::mpsc,
-};
-
-use crate::cli::Args;
-use crate::state::ScreenReaderState;
 use odilia_input::sr_event_receiver;
 use odilia_notify::listen_to_dbus_notifications;
 use ssip_client_async::Priority;
+use tokio::{
+	signal::unix::{signal, SignalKind},
+	sync::mpsc,
+};
+use tokio_util::{sync::CancellationToken, task::TaskTracker};
 
 use atspi_common::events::{document, object};
 async fn notifications_monitor(
 	state: Arc<ScreenReaderState>,
-	shutdown_rx: &mut broadcast::Receiver<i32>,
+	shutdown: CancellationToken,
 ) -> eyre::Result<()> {
 	let mut stream = listen_to_dbus_notifications().await?;
 	loop {
@@ -43,7 +42,7 @@ async fn notifications_monitor(
 			format!("new notification: {}, {}.", notification.title, notification.body);
 		      state.say(Priority::Important, notification_message).await;
 		    },
-		    _ = shutdown_rx.recv() => {
+		    () = shutdown.cancelled() => {
 		      tracing::debug!("Shutting down notification task.");
 		      break;
 		    },
@@ -51,36 +50,49 @@ async fn notifications_monitor(
 	}
 	Ok(())
 }
-async fn sigterm_signal_watcher(shutdown_tx: broadcast::Sender<i32>) -> eyre::Result<()> {
+async fn sigterm_signal_watcher(
+	token: CancellationToken,
+	tracker: TaskTracker,
+) -> eyre::Result<()> {
 	let mut c = signal(SignalKind::interrupt())?;
 	tracing::debug!("Watching for Ctrl+C");
 	c.recv().await;
 	tracing::debug!("Asking all processes to stop.");
-	let _: Result<usize, SendError<i32>> = shutdown_tx.send(0);
+	tracing::debug!("cancelling all tokens");
+	token.cancel();
+	tracing::debug!("waiting for all tasks to finish");
+	tracker.close();
+	tracing::debug!("All listeners have stopped.");
+	tracing::debug!("Goodbye, Odilia!");
 	Ok(())
 }
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> eyre::Result<()> {
 	let args = Args::parse();
-
 	logging::init();
+
+	//initialize the primary token for task cancelation
+	let token = CancellationToken::new();
+
+	//initialize a task tracker, which will allow us to wait for all tasks to finish
+	let tracker = TaskTracker::new();
+
 	// Make sure applications with dynamic accessibility supprt do expose their AT-SPI2 interfaces.
 	if let Err(e) = atspi_connection::set_session_accessibility(true).await {
 		tracing::debug!("Could not set AT-SPI2 IsEnabled property because: {}", e);
 	}
-	let (shutdown_tx, _) = broadcast::channel(1);
-	let (sr_event_tx, mut sr_event_rx) = mpsc::channel(128);
+	let (sr_event_tx, sr_event_rx) = mpsc::channel(128);
 	// this channel must NEVER fill up; it will cause the thread receiving events to deadlock due to a zbus design choice.
 	// If you need to make it bigger, then make it bigger, but do NOT let it ever fill up.
-	let (atspi_event_tx, mut atspi_event_rx) = mpsc::channel(128);
+	let (atspi_event_tx, atspi_event_rx) = mpsc::channel(128);
 	// this is the chanel which handles all SSIP commands. If SSIP is not allowed to operate on a separate task, then wdaiting for the receiving message can block other long-running operations like structural navigation.
 	// Although in the future, this may possibly be remidied through a proper cache, I think it still makes sense to separate SSIP's IO operations to a separate task.
 	// Like the channel above, it is very important that this is *never* full, since it can cause deadlocking if the other task sending the request is working with zbus.
-	let (ssip_req_tx, ssip_req_rx) = mpsc::channel::<ssip_client_async::tokio::Request>(128);
+	let (ssip_req_tx, ssip_req_rx) = mpsc::channel::<ssip_client_async::Request>(128);
 	// Initialize state
 	let state = Arc::new(ScreenReaderState::new(ssip_req_tx, args.config.as_deref()).await?);
-	let mut ssip = odilia_tts::create_ssip_client().await?;
+	let ssip = odilia_tts::create_ssip_client().await?;
 
 	if state.say(Priority::Message, "Welcome to Odilia!".to_string()).await {
 		tracing::debug!("Welcome message spoken.");
@@ -100,50 +112,32 @@ async fn main() -> eyre::Result<()> {
 		state.add_cache_match_rule(),
 	)?;
 
-	let mut shutdown_rx_ssip_recv = shutdown_tx.subscribe();
-	let ssip_event_receiver = odilia_tts::handle_ssip_commands(
-		&mut ssip,
-		ssip_req_rx,
-		&mut shutdown_rx_ssip_recv,
-	)
-	.map(|r| r.wrap_err("Could no process SSIP request"));
-	let mut shutdown_rx_atspi_recv = shutdown_tx.subscribe();
+	let ssip_event_receiver =
+		odilia_tts::handle_ssip_commands(ssip, ssip_req_rx, token.clone())
+			.map(|r| r.wrap_err("Could no process SSIP request"));
 	let atspi_event_receiver =
-		events::receive(Arc::clone(&state), atspi_event_tx, &mut shutdown_rx_atspi_recv)
+		events::receive(Arc::clone(&state), atspi_event_tx, token.clone())
 			.map(|()| Ok::<_, eyre::Report>(()));
-	let mut shutdown_rx_atspi_proc_recv = shutdown_tx.subscribe();
-	let atspi_event_processor = events::process(
-		Arc::clone(&state),
-		&mut atspi_event_rx,
-		&mut shutdown_rx_atspi_proc_recv,
-	)
-	.map(|()| Ok::<_, eyre::Report>(()));
-	let mut shutdown_rx_odilia_recv = shutdown_tx.subscribe();
-	let odilia_event_receiver = sr_event_receiver(sr_event_tx, &mut shutdown_rx_odilia_recv)
+	let atspi_event_processor =
+		events::process(Arc::clone(&state), atspi_event_rx, token.clone())
+			.map(|()| Ok::<_, eyre::Report>(()));
+	let odilia_event_receiver = sr_event_receiver(sr_event_tx, token.clone())
 		.map(|r| r.wrap_err("Could not process Odilia events"));
-	let mut shutdown_rx_odilia_proc_recv = shutdown_tx.subscribe();
-	let odilia_event_processor = events::sr_event(
-		Arc::clone(&state),
-		&mut sr_event_rx,
-		&mut shutdown_rx_odilia_proc_recv,
-	)
-	.map(|r| r.wrap_err("Could not process Odilia event"));
-	let mut shutdown_rx_notif = shutdown_tx.subscribe();
-	let notification_task = notifications_monitor(Arc::clone(&state), &mut shutdown_rx_notif)
-		.map(|r| r.wrap_err("Could not process signal shutdown."));
-	let signal_receiver = sigterm_signal_watcher(shutdown_tx)
+	let odilia_event_processor =
+		events::sr_event(Arc::clone(&state), sr_event_rx, token.clone())
+			.map(|r| r.wrap_err("Could not process Odilia event"));
+	let notification_task = notifications_monitor(Arc::clone(&state), token.clone())
 		.map(|r| r.wrap_err("Could not process signal shutdown."));
 
-	tokio::try_join!(
-		signal_receiver,
-		atspi_event_receiver,
-		atspi_event_processor,
-		odilia_event_receiver,
-		odilia_event_processor,
-		ssip_event_receiver,
-		notification_task,
-	)?;
-	tracing::debug!("All listeners have stopped.");
-	tracing::debug!("Goodbye, Odilia!");
+	tracker.spawn(atspi_event_receiver);
+	tracker.spawn(atspi_event_processor);
+	tracker.spawn(odilia_event_receiver);
+	tracker.spawn(odilia_event_processor);
+	tracker.spawn(ssip_event_receiver);
+	tracker.spawn(notification_task);
+	tracker.close();
+	let _ = sigterm_signal_watcher(token, tracker)
+		.await
+		.wrap_err("can not process interrupt signal");
 	Ok(())
 }
