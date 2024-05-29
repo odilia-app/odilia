382b48e2d85e49a1760b63f496e814b904f23630
Tait Hoyem <tait@tait.tech>
2024-05-29 12:18:05 -0600

    Add IntoCommands trait, and implement it for a tuple up to three

--- common/src/command.rs
+++ common/src/command.rs
@@ -1,6 +1,58 @@
+use ssip::Priority;
 use enum_dispatch::enum_dispatch;
 
 use strum::{Display, EnumDiscriminants};
+
+pub trait IntoCommands {
+	fn into_commands(self) -> Vec<OdiliaCommand>;
+}
+impl From<Priority> for OdiliaCommand {
+	fn from(p: Priority) -> OdiliaCommand {
+		SpeechPriority(p).into()
+	}
+}
+impl From<String> for OdiliaCommand {
+	fn from(s: String) -> OdiliaCommand {
+		Speak(s).into()
+	}
+}
+impl<T1> IntoCommands for T1
+where T1: Into<OdiliaCommand> {
+	fn into_commands(self) -> Vec<OdiliaCommand> {
+		vec![self.into()]
+	}
+}
+impl<T1> IntoCommands for (T1,)
+where T1: Into<OdiliaCommand> {
+	fn into_commands(self) -> Vec<OdiliaCommand> {
+		vec![self.0.into()]
+	}
+}
+impl<T1,T2> IntoCommands for (T1,T2,)
+where T1: Into<OdiliaCommand>,
+			T2: Into<OdiliaCommand> {
+	fn into_commands(self) -> Vec<OdiliaCommand> {
+		vec![self.0.into(), self.1.into()]
+	}
+}
+impl<T1,T2,T3> IntoCommands for (T1,T2,T3,)
+where T1: Into<OdiliaCommand>,
+			T2: Into<OdiliaCommand>,
+			T3: Into<OdiliaCommand> {
+	fn into_commands(self) -> Vec<OdiliaCommand> {
+		vec![self.0.into(), self.1.into(), self.2.into()]
+	}
+}
+impl<T1,T2,T3,T4> IntoCommands for (T1,T2,T3,T4,)
+where T1: Into<OdiliaCommand>,
+			T2: Into<OdiliaCommand>,
+			T3: Into<OdiliaCommand>,
+			T4: Into<OdiliaCommand> {
+	fn into_commands(self) -> Vec<OdiliaCommand> {
+		vec![self.0.into(), self.1.into(), self.2.into(), self.3.into()]
+	}
+}
+
 
 pub trait CommandType {
 	const CTYPE: OdiliaCommandDiscriminants;
@@ -17,8 +69,15 @@ impl<T: CommandType> CommandTypeDynamic for T {
 
 #[derive(Debug, Clone)]
 pub struct Speak(pub String);
+
+#[derive(Debug, Clone)]
+pub struct SpeechPriority(pub Priority);
+
 impl CommandType for Speak {
 	const CTYPE: OdiliaCommandDiscriminants = OdiliaCommandDiscriminants::Speak;
+}
+impl CommandType for SpeechPriority {
+	const CTYPE: OdiliaCommandDiscriminants = OdiliaCommandDiscriminants::SpeechPriority;
 }
 
 #[derive(Debug, Clone, EnumDiscriminants)]
@@ -26,4 +85,5 @@ impl CommandType for Speak {
 #[enum_dispatch(CommandTypeDynamic)]
 pub enum OdiliaCommand {
 	Speak(Speak),
+	SpeechPriority(SpeechPriority),
 }
