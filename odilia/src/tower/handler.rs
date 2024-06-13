e31e38778e0c9b36b57248bd038dcdfa02cb5847
Tait Hoyem <tait@tait.tech>
2024-06-13 13:20:08 -0600

    Rework Handler function to be much more generic

--- odilia/src/tower/handler.rs
+++ odilia/src/tower/handler.rs
@@ -13,60 +13,36 @@ use tower::Service;
 
 type Request = Event;
 
-pub trait Handler<T, E> {
+pub trait Handler<T> {
 	type Response;
 	type Future: Future<Output = Self::Response>;
-	fn into_service<R>(self) -> HandlerService<Self, T, E, R>
+	fn into_service<R>(self) -> HandlerService<Self, T, R>
 	where
 		Self: Sized,
 	{
 		HandlerService::new(self)
 	}
-	fn call(self, req: E, params: T) -> Self::Future;
-}
-
-impl<F, Fut, E, R> Handler<((),), E> for F
-where
-	F: FnOnce() -> Fut,
-	Fut: Future<Output = R>,
-{
-	type Response = R;
-	type Future = Fut;
-	fn call(self, _req: E, _param: ((),)) -> Self::Future {
-		self()
-	}
-}
-
-impl<F, Fut, E, R> Handler<(Request,), E> for F
-where
-	F: FnOnce(E) -> Fut,
-	Fut: Future<Output = R>,
-{
-	type Response = R;
-	type Future = Fut;
-	fn call(self, req: E, _params: (Request,)) -> Self::Future {
-		self(req)
-	}
+	fn call(self, params: T) -> Self::Future;
 }
 
 macro_rules! impl_handler {
     ($($type:ident,)+) => {
         #[allow(non_snake_case)]
-        impl<F, Fut, E, R, $($type,)+> Handler<($($type,)+), E> for F
+        impl<F, Fut, R, $($type,)+> Handler<($($type,)+)> for F
         where
-            F: FnOnce(E, $($type,)+) -> Fut + Send,
+            F: FnOnce($($type,)+) -> Fut + Send,
             Fut: Future<Output = R> + Send,
-            $($type: Send,)+
-            E: Send {
+            $($type: Send,)+ {
       type Response = R;
       type Future = impl Future<Output = R>;
-      fn call(self, req: E, params: ($($type,)+)) -> Self::Future {
+      fn call(self, params: ($($type,)+)) -> Self::Future {
           let ($($type,)+) = params;
-          self(req, $($type,)+)
+          self($($type,)+)
       }
     }
 }
 }
+impl_handler!(T1,);
 impl_handler!(T1, T2,);
 impl_handler!(T1, T2, T3,);
 impl_handler!(T1, T2, T3, T4,);
@@ -75,11 +51,11 @@ impl_handler!(T1, T2, T3, T4, T5, T6,);
 impl_handler!(T1, T2, T3, T4, T5, T6, T7,);
 
 #[allow(clippy::type_complexity)]
-pub struct HandlerService<H, T, E, R> {
+pub struct HandlerService<H, T, R> {
 	handler: H,
-	_marker: PhantomData<fn(E, T) -> R>,
+	_marker: PhantomData<fn(T) -> R>,
 }
-impl<H, T, E, R> Clone for HandlerService<H, T, E, R>
+impl<H, T, R> Clone for HandlerService<H, T, R>
 where
 	H: Clone,
 {
@@ -87,18 +63,18 @@ where
 		HandlerService { handler: self.handler.clone(), _marker: PhantomData }
 	}
 }
-impl<H, T, E, R> HandlerService<H, T, E, R> {
+impl<H, T, R> HandlerService<H, T, R> {
 	fn new(handler: H) -> Self
 	where
-		H: Handler<T, E>,
+		H: Handler<T>,
 	{
 		HandlerService { handler, _marker: PhantomData }
 	}
 }
 
-impl<H, T, E, R> Service<(E, T)> for HandlerService<H, T, E, R>
+impl<H, T, R> Service<T> for HandlerService<H, T, R>
 where
-	H: Handler<T, E> + Clone,
+	H: Handler<T> + Clone,
 {
 	type Response = H::Response;
 	type Future = impl Future<Output = Result<H::Response, Infallible>>;
@@ -107,7 +83,7 @@ where
 	fn poll_ready(&mut self, _ctx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
 		Poll::Ready(Ok(()))
 	}
-	fn call(&mut self, params: (E, T)) -> Self::Future {
-		self.handler.clone().call(params.0, params.1).map(|o| Ok(o))
+	fn call(&mut self, params: T) -> Self::Future {
+		self.handler.clone().call(params).map(|o| Ok(o))
 	}
 }
