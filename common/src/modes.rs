cfc97c1deab8f37c0d0c54529f463d3c6b51342d
Tait Hoyem <tait@tait.tech>
2025-01-22 17:00:27 -0700

    Make mode an exaustive enum

--- common/src/modes.rs
+++ common/src/modes.rs
@@ -1,13 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, PartialEq, Debug, Eq, Hash, Serialize, Deserialize)]
-pub struct ScreenReaderMode {
-	pub name: String,
-}
-
-impl ScreenReaderMode {
-	#[must_use]
-	pub fn new(name: &str) -> Self {
-		ScreenReaderMode { name: name.to_string() }
-	}
+pub enum ScreenReaderMode {
+	Focus,
+	Browse,
 }
