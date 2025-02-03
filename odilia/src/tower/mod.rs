dd4ce073333d46a78b3ece25580a752e8ae87a00
Tait Hoyem <tait@tait.tech>
2025-02-03 15:33:39 -0700

    Use tower-iter's IterService

--- odilia/src/tower/mod.rs
+++ odilia/src/tower/mod.rs
@@ -4,7 +4,7 @@ pub mod extractors;
 pub use extractors::*;
 pub mod from_state;
 pub mod handler;
-pub mod iter_svc;
+pub use tower_iter::iter_svc;
 pub mod service_ext;
 pub use tower_iter::service_set;
 pub mod state_changed;
