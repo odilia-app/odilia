cdc9d308930ccb8dabcebbb77f6be3b841f8a0c4
Tait Hoyem <tait@tait.tech>
2025-02-03 15:35:33 -0700

    Use generic iterators for commands instead of allocating every time

--- common/src/command.rs
+++ common/src/command.rs
@@ -4,36 +4,60 @@ use crate::cache::AccessiblePrimitive;
 use crate::errors::OdiliaError;
 use enum_dispatch::enum_dispatch;
 use ssip::Priority;
+use std::array::IntoIter;
 use std::convert::Infallible;
+use std::iter::Chain;
 
 use strum::{Display, EnumDiscriminants};
 
 pub trait TryIntoCommands {
 	type Error: Into<OdiliaError>;
+	type Iter: Iterator<Item = OdiliaCommand> + Send;
 	/// Fallibly returns a [`Vec`] of [`OdiliaCommand`]s to run.
+	/// This collects the commands returned from [`try_into_commands`] and returns them upon
+	/// successful evaluation.
 	///
 	/// # Errors
 	///
 	/// When implemented, the function is allowed to fail with any type that can be converted into
 	/// [`OdiliaError`], but conversion should between these types should be done from the
 	/// implementers' side, liekly using `?`.
-	fn try_into_commands(self) -> Result<Vec<OdiliaCommand>, OdiliaError>;
+	fn try_into_commands_collected(self) -> Result<Vec<OdiliaCommand>, OdiliaError>
+	where
+		Self: Sized,
+	{
+		match self.try_into_commands() {
+			Ok(vec) => Ok(vec.collect()),
+			Err(e) => Err(e),
+		}
+	}
+	/// Fallibly returns an iterator of [`OdiliaCommand`]s to run.
+	///
+	/// # Errors
+	///
+	/// When implemented, the function is allowed to fail with any type that can be converted into
+	/// [`OdiliaError`], but conversion should between these types should be done from the
+	/// implementers' side, liekly using `?`.
+	fn try_into_commands(self) -> Result<Self::Iter, OdiliaError>;
 }
 impl TryIntoCommands for Result<Vec<OdiliaCommand>, OdiliaError> {
 	type Error = OdiliaError;
-	fn try_into_commands(self) -> Result<Vec<OdiliaCommand>, OdiliaError> {
-		self
+	type Iter = std::vec::IntoIter<OdiliaCommand>;
+	fn try_into_commands(self) -> Result<Self::Iter, OdiliaError> {
+		self.map(|vec| vec.into_iter())
 	}
 }
 impl<T: IntoCommands> TryIntoCommands for T {
 	type Error = Infallible;
-	fn try_into_commands(self) -> Result<Vec<OdiliaCommand>, OdiliaError> {
+	type Iter = T::Iter;
+	fn try_into_commands(self) -> Result<Self::Iter, OdiliaError> {
 		Ok(self.into_commands())
 	}
 }
 impl<T: IntoCommands, E: Into<OdiliaError>> TryIntoCommands for Result<T, E> {
 	type Error = E;
-	fn try_into_commands(self) -> Result<Vec<OdiliaCommand>, OdiliaError> {
+	type Iter = T::Iter;
+	fn try_into_commands(self) -> Result<Self::Iter, OdiliaError> {
 		match self {
 			Ok(ok) => Ok(ok.into_commands()),
 			Err(err) => Err(err.into()),
@@ -42,39 +66,46 @@ impl<T: IntoCommands, E: Into<OdiliaError>> TryIntoCommands for Result<T, E> {
 }
 
 pub trait IntoCommands {
-	fn into_commands(self) -> Vec<OdiliaCommand>;
+	type Iter: Iterator<Item = OdiliaCommand> + Send;
+	fn into_commands(self) -> Self::Iter;
 }
 
 impl IntoCommands for CaretPos {
-	fn into_commands(self) -> Vec<OdiliaCommand> {
-		vec![self.into()]
+	type Iter = IntoIter<OdiliaCommand, 1>;
+	fn into_commands(self) -> Self::Iter {
+		[self.into()].into_iter()
 	}
 }
 impl IntoCommands for Focus {
-	fn into_commands(self) -> Vec<OdiliaCommand> {
-		vec![self.into()]
+	type Iter = IntoIter<OdiliaCommand, 1>;
+	fn into_commands(self) -> Self::Iter {
+		[self.into()].into_iter()
 	}
 }
 impl IntoCommands for (Priority, &str) {
-	fn into_commands(self) -> Vec<OdiliaCommand> {
-		vec![Speak(self.1.to_string(), self.0).into()]
+	type Iter = IntoIter<OdiliaCommand, 1>;
+	fn into_commands(self) -> Self::Iter {
+		[Speak(self.1.to_string(), self.0).into()].into_iter()
 	}
 }
 impl IntoCommands for (Priority, String) {
-	fn into_commands(self) -> Vec<OdiliaCommand> {
-		vec![Speak(self.1, self.0).into()]
+	type Iter = IntoIter<OdiliaCommand, 1>;
+	fn into_commands(self) -> Self::Iter {
+		[Speak(self.1, self.0).into()].into_iter()
 	}
 }
 impl IntoCommands for () {
-	fn into_commands(self) -> Vec<OdiliaCommand> {
-		vec![]
+	type Iter = IntoIter<OdiliaCommand, 0>;
+	fn into_commands(self) -> Self::Iter {
+		[].into_iter()
 	}
 }
 impl<T1> IntoCommands for (T1,)
 where
 	T1: IntoCommands,
 {
-	fn into_commands(self) -> Vec<OdiliaCommand> {
+	type Iter = T1::Iter;
+	fn into_commands(self) -> Self::Iter {
 		self.0.into_commands()
 	}
 }
@@ -83,10 +114,9 @@ where
 	T1: IntoCommands,
 	T2: IntoCommands,
 {
-	fn into_commands(self) -> Vec<OdiliaCommand> {
-		let mut ret = self.0.into_commands();
-		ret.extend(self.1.into_commands());
-		ret
+	type Iter = Chain<T1::Iter, T2::Iter>;
+	fn into_commands(self) -> Self::Iter {
+		self.0.into_commands().chain(self.1.into_commands())
 	}
 }
 impl<T1, T2, T3> IntoCommands for (T1, T2, T3)
@@ -95,11 +125,11 @@ where
 	T2: IntoCommands,
 	T3: IntoCommands,
 {
-	fn into_commands(self) -> Vec<OdiliaCommand> {
-		let mut ret = self.0.into_commands();
-		ret.extend(self.1.into_commands());
-		ret.extend(self.2.into_commands());
-		ret
+	type Iter = Chain<Chain<T1::Iter, T2::Iter>, T3::Iter>;
+	fn into_commands(self) -> Self::Iter {
+		self.0.into_commands()
+			.chain(self.1.into_commands())
+			.chain(self.2.into_commands())
 	}
 }
 impl<T1, T2, T3, T4> IntoCommands for (T1, T2, T3, T4)
@@ -109,12 +139,12 @@ where
 	T3: IntoCommands,
 	T4: IntoCommands,
 {
-	fn into_commands(self) -> Vec<OdiliaCommand> {
-		let mut ret = self.0.into_commands();
-		ret.extend(self.1.into_commands());
-		ret.extend(self.2.into_commands());
-		ret.extend(self.3.into_commands());
-		ret
+	type Iter = Chain<Chain<Chain<T1::Iter, T2::Iter>, T3::Iter>, T4::Iter>;
+	fn into_commands(self) -> Self::Iter {
+		self.0.into_commands()
+			.chain(self.1.into_commands())
+			.chain(self.2.into_commands())
+			.chain(self.3.into_commands())
 	}
 }
 
