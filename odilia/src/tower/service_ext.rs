ab9eae79548509e2370b56464eaf7093522ac472
Tait Hoyem <tait@tait.tech>
2025-02-03 20:16:17 -0700

    Add new combinators, stricten type bounds

--- odilia/src/tower/service_ext.rs
+++ odilia/src/tower/service_ext.rs
@@ -1,34 +1,42 @@
 use crate::tower::{
-	async_try::{AsyncTryInto, AsyncTryIntoLayer, AsyncTryIntoService},
+	async_try::{AsyncTryFrom, AsyncTryInto, AsyncTryIntoLayer, AsyncTryIntoService},
 	iter_svc::IterService,
 	state_svc::{StateLayer, StateService},
 	sync_try::{TryIntoLayer, TryIntoService},
-	unwrap_svc::{MapErrIntoService, MapResponseIntoService, UnwrapService},
+	unwrap_svc::{
+		MapErrIntoService, MapResponseIntoService, MapResponseTryIntoCommandsService,
+		UnwrapService,
+	},
 };
+use crate::TryIntoCommands;
 use std::{convert::Infallible, sync::Arc};
 use tower::{Layer, Service};
 
 pub trait ServiceExt<Request>: Service<Request> {
-	fn request_try_from<I, R, Fut1>(self) -> TryIntoService<Request, I, Self, R, Fut1>
+	fn request_try_from<I, R, Fut1, E>(self) -> TryIntoService<Request, I, Self, R, Fut1>
 	where
-		Self: Sized,
+		Self: Service<Request, Response = R, Future = Fut1> + Sized,
 		I: TryInto<Request>,
-		Self: Service<Request, Response = R, Future = Fut1>,
+		E: From<<I as TryInto<Request>>::Error>,
+		Fut1: futures::future::Future<Output = Result<R, E>>,
 	{
 		TryIntoLayer::new().layer(self)
 	}
-	fn request_async_try_from<I, R, Fut1>(
+	fn request_async_try_from<I, R, Fut1, E, E2>(
 		self,
 	) -> AsyncTryIntoService<Request, I, Self, R, Fut1>
 	where
-		I: AsyncTryInto<Request>,
-		Self: Service<Request, Response = R, Future = Fut1> + Clone,
+		Self: Service<Request, Response = R, Future = Fut1, Error = E> + Clone + Sized,
+		I: AsyncTryInto<Request, Error = E2>,
+		E: From<E2>,
+		Fut1: std::future::Future<Output = Result<R, E>>,
 	{
 		AsyncTryIntoLayer::new().layer(self)
 	}
 	fn with_state<S>(self, s: Arc<S>) -> StateService<Self, S>
 	where
-		Self: Sized,
+		Self: Service<Request> + Clone + Sized,
+		StateLayer<S>: Layer<Self, Service = StateService<Self, S>>,
 	{
 		StateLayer::new(s).layer(self)
 	}
@@ -40,14 +48,19 @@ pub trait ServiceExt<Request>: Service<Request> {
 		UnwrapService::new(self, f)
 	}
 
-	fn map_response_into<Res, R, E, T>(
-		self,
-	) -> MapResponseIntoService<Self, Request, Res, R, E, T>
+	fn map_response_into<Res, R, E>(self) -> MapResponseIntoService<Self, Request, Res, R, E>
 	where
 		Self: Service<Request, Response = Res, Error = Infallible> + Sized,
 		Res: Into<Result<R, E>>,
 	{
 		MapResponseIntoService::new(self)
+	}
+	fn map_response_try_into_command(self) -> MapResponseTryIntoCommandsService<Self, Request>
+	where
+		Self: Service<Request, Error = Infallible> + Sized,
+		<Self as Service<Request>>::Response: TryIntoCommands,
+	{
+		MapResponseTryIntoCommandsService::new(self)
 	}
 	fn map_err_into<Req, E1, R, E, T>(self) -> MapErrIntoService<Self, Req, E1, R, E, T>
 	where
