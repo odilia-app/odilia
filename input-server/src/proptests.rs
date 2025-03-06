64dbfd10aa8181b71a2aa2aea711745f731e89fa
Tait Hoyem <tait@tait.tech>
2025-03-06 00:51:24 -0700

    Remove repeated tests from tests.rs

--- input-server/src/proptests.rs
+++ input-server/src/proptests.rs
@@ -1,5 +1,5 @@
 use crate::{
-	callback, ComboError, ComboSet, ComboSets, KeySet, Mode, OdiliaEvent, Receiver, SetError,
+	callback, ComboSet, ComboSets, EventFromEventType, KeySet, Mode, OdiliaEvent, Receiver,
 	State, ACTIVATION_KEY,
 };
 use atspi_common::Role;
@@ -8,13 +8,6 @@ use proptest::prelude::*;
 use rdev::{Button, Event, EventType, Key};
 use std::sync::mpsc::TryRecvError;
 use std::time::SystemTime;
-
-trait EventFromEventType {
-	fn from_event_type(event_type: EventType) -> Event {
-		Event { event_type, time: SystemTime::now(), name: None }
-	}
-}
-impl EventFromEventType for Event {}
 
 fn key() -> impl Strategy<Value = Key> {
 	prop_oneof![
@@ -459,46 +452,4 @@ proptest! {
 		callback(ev, &mut state);
 	    });
 		}
-}
-
-#[test]
-fn test_unreachable_mode() {
-	let core_combos = ComboSet::from_iter(
-		vec![(vec![Key::KeyA].try_into().unwrap(), ChangeMode(Mode::Browse).into())]
-			.into_iter(),
-	);
-	let focus_combos = ComboSet::from_iter(
-		vec![(vec![Key::KeyP].try_into().unwrap(), StopSpeech.into())].into_iter(),
-	);
-	let combos = ComboSets::try_from([(None, core_combos), (Some(Mode::Focus), focus_combos)]);
-	assert_eq!(combos, Err(SetError::UnreachableMode(Mode::Focus)), "It should not be possible to construct the ComboSet when there is no way to activate that mode!");
-}
-
-#[test]
-fn test_same_prefix() {
-	let core_combos = ComboSet::from_iter(
-		vec![
-			(
-				vec![Key::ShiftLeft, Key::KeyA].try_into().unwrap(),
-				ChangeMode(Mode::Browse).into(),
-			),
-			(
-				vec![Key::ShiftLeft, Key::KeyP].try_into().unwrap(),
-				ChangeMode(Mode::Browse).into(),
-			),
-		]
-		.into_iter(),
-	);
-	ComboSets::try_from([(None, core_combos)])
-		.expect("Able to construct two bindings with the same prefix!");
-}
-
-#[test]
-fn test_two_bindings_same_keys() {
-	let shift_plus_a: KeySet = vec![Key::ShiftLeft, Key::KeyA].try_into().unwrap();
-	let core_combos = ComboSet::try_from(vec![
-		(shift_plus_a.clone(), ChangeMode(Mode::Browse).into()),
-		(shift_plus_a.clone(), ChangeMode(Mode::Focus).into()),
-	]);
-	assert_eq!(core_combos, Err(ComboError::Identical(shift_plus_a)), "You should not be able to construct two key bindings in the same mode with identical keystrokes!");
 }
