9ab36dddcafb778832b4d4f7421a0c5bfa2eb312
Tait Hoyem <tait@tait.tech>
2025-03-19 12:20:11 -0600

    [WIP]

--- common/src/command.rs
+++ common/src/command.rs
@@ -1,5 +1,6 @@
 #![allow(clippy::module_name_repetitions)]
 
+use zbus::zvariant::Type;
 use crate::cache::AccessiblePrimitive;
 use crate::errors::OdiliaError;
 use enum_dispatch::enum_dispatch;
@@ -146,13 +147,13 @@ impl<T: CommandType> CommandTypeDynamic for T {
 	}
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Type)]
 pub struct CaretPos(pub usize);
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Type)]
 pub struct Speak(pub String, pub Priority);
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Type)]
 pub struct Focus(pub AccessiblePrimitive);
 
 macro_rules! impl_command_type {
@@ -167,7 +168,7 @@ impl_command_type!(Focus, Focus);
 impl_command_type!(Speak, Speak);
 impl_command_type!(CaretPos, CaretPos);
 
-#[derive(Debug, Clone, EnumDiscriminants)]
+#[derive(Debug, Clone, EnumDiscriminants, Type)]
 #[strum_discriminants(derive(Ord, PartialOrd, Display))]
 #[enum_dispatch(CommandTypeDynamic)]
 pub enum OdiliaCommand {
