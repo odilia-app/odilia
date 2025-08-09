75bbd5b0803a8367a4ec262549dc71bf45715698
Tait Hoyem <tait@tait.tech>
2025-08-09 10:37:31 -0600

    Move handers removed from cmds

--- odilia/src/handlers/input.rs
+++ odilia/src/handlers/input.rs
@@ -1,4 +1,7 @@
-use odilia_common::{command::TryIntoCommands, events::ChangeMode};
+use odilia_common::{
+	command::{Quit as QuitCommand, TryIntoCommands},
+	events::{ChangeMode, Quit, StopSpeech, StructuralNavigation},
+};
 use ssip::Priority;
 
 use crate::InputEvent;
@@ -6,4 +9,21 @@ use crate::InputEvent;
 #[tracing::instrument(ret)]
 pub async fn change_mode(InputEvent(cm): InputEvent<ChangeMode>) -> impl TryIntoCommands {
 	(Priority::Text, format!("{:?} mode", cm.0))
+}
+
+#[tracing::instrument(ret)]
+pub async fn quit_input(InputEvent(cm): InputEvent<Quit>) -> impl TryIntoCommands {
+	QuitCommand
+}
+
+#[tracing::instrument(ret)]
+pub async fn stop_speech(InputEvent(_): InputEvent<StopSpeech>) -> impl TryIntoCommands {
+	(Priority::Text, "Stop speech")
+}
+
+#[tracing::instrument(ret)]
+pub async fn structural_nav(
+	InputEvent(sn): InputEvent<StructuralNavigation>,
+) -> impl TryIntoCommands {
+	(Priority::Text, format!("Navigate to {}, {:?}", sn.1, sn.0))
 }
